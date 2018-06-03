e195ce79e8411dea23e7754d4fe03c4c55c11e18
frsab <frejsaber@gmail.com>
2018-06-03 20:56:02 +0200

    patch

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 exports.printMsg = function() {
   console.log("This is a message from the demo package");
 }
-exports.runLocal = function() {
-const cluster = require('cluster');
-const http = require('http');
-const numCPUs = require('os').cpus().length;
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+exports.runLocal = function(f:function) {
+	const cluster = require('cluster');
+	const http = require('http');
+	const numCPUs = require('os').cpus().length;
+	if(f){
+		f();
+	}
+	if (cluster.isMaster) {
+	  console.log(`Master ${process.pid} is running`);
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+	  // Fork workers.
+	  for (let i = 0; i < numCPUs; i++) {
+	  	console.log('fork');
+	    cluster.fork();
+	  }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-} else {
-  // Workers can share any TCP connection
-  // In this case it is an HTTP server
-  http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('hello world\n');
-  }).listen(8000);
+	  cluster.on('exit', (worker, code, signal) => {
+	    console.log(`worker ${worker.process.pid} died`);
+	  });
+	} else {
+	  // Workers can share any TCP connection
+	  // In this case it is an HTTP server
+	  http.createServer((req, res) => {
+	    res.writeHead(200);
+	    res.end('hello world\n');
+	  }).listen(8000);
 
-  console.log(`Worker ${process.pid} started`);
-}
+	  console.log(`Worker ${process.pid} started`);
+	}
 }
 
 
